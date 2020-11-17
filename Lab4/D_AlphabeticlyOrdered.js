936539c424335d10f5251bf2b1a9cedff19a0cd9
ozbekleromer <45530887+ozbekleromer@users.noreply.github.com>
2020-11-17 07:59:54 +0100

    Create D_AlphabeticlyOrdered.js

---
+++ Lab4/D_AlphabeticlyOrdered.js
@@ -0,0 +1,13 @@
+function ordered(str) {
+	if(typeof str != "string")	
+  	return "ERROR - Input must string!"
+  return str == str.split("").sort().join("");
+}
+
+console.log(ordered());
+console.log(ordered(""));
+console.log(ordered(undefined));
+console.log(ordered("dsxz"));
+console.log(ordered("abcdf"));
+console.log(ordered("aasdfasfga"));
+console.log(ordered("qwertyu"));
