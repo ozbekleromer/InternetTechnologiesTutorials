3ce9e278a68bc7535152795f9b4caf9201fe64b9
ozbekleromer <45530887+ozbekleromer@users.noreply.github.com>
2020-12-06 21:26:13 +0100

    Create F_Extends.js

---
+++ Lab5/F_Extends.js
@@ -0,0 +1,41 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+  }
+
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  set fullName(name) {
+    var words = name.toString().split(' ');
+    this.firstName = words[0] || '';
+    this.lastName = words[1] || '';
+  }
+}
+
+class Student extends Person {
+
+  constructor(firstName, lastName, id, grades) {
+    super(firstName, lastName);
+    this.id = id;
+    this.grades = grades;
+  }
+
+  get average() {
+    let total = this.grades.reduce((sum, num) => sum + num);
+    let count = this.grades.length;
+    return total / count;
+  }
+}
+
+var studentOne = new Student("Ali", "Oz", "S19589", [3, 3.5, 4, 5, 3, 4.5]);
+var studentTwo = new Student("Nur", "Oz", "S19845", [4, 4.5, 4, 5, 3, 4.5]);
+
+console.log(studentOne.fullName + ": " + studentOne.average);
+console.log(studentTwo.fullName + ": " + studentTwo.average);
+studentOne.fullName = "Omer Ozbek";
+console.log(studentOne.fullName + ": " + studentOne.average);
+console.log(studentTwo.fullName + ": " + studentTwo.average);
